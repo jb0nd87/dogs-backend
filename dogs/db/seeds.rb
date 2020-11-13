66143b23d9f1cc152e0d13f249a5fa428af2e510
jb0nd87 <68964150+jb0nd87@users.noreply.github.com>
2020-11-13 13:53:36 -0800

    dogs seeded update

--- dogs/db/seeds.rb
+++ dogs/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create(name: "Sparky", age: 5, breed: "Bulldog")
+Dog.create(name: "Spot", age: 5, breed: "Dalmation")
+Dog.create(name: "Goldy", age: 5, breed: "Golden Retriever")
